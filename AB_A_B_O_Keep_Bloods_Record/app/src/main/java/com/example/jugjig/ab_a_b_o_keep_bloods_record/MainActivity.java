eadc9bf99c0d1256817bdf23a909f98b0b45efae
JugJig <59070027@it.kmitl.ac.th>
2018-10-10 15:00:51 +0700

    New Project

---
+++ AB_A_B_O_Keep_Bloods_Record/app/src/main/java/com/example/jugjig/ab_a_b_o_keep_bloods_record/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.jugjig.ab_a_b_o_keep_bloods_record;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
