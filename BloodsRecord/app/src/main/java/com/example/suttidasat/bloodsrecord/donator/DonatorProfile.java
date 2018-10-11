56c12b0f8f5beedac23ebf4761b7e2eb2eede6ef
JugJig <59070027@it.kmitl.ac.th>
2018-10-12 00:14:17 +0700

    try to set donator profile

---
+++ BloodsRecord/app/src/main/java/com/example/suttidasat/bloodsrecord/donator/DonatorProfile.java
@@ -0,0 +1,71 @@
+package com.example.suttidasat.bloodsrecord.donator;
+
+public class DonatorProfile {
+
+    private String birth;
+    private String fName;
+    private String lName;
+    private String nationalID;
+    private String email;
+    private String bloodGroup;
+
+    public DonatorProfile() {
+    }
+
+    public String getBirth() {
+        return birth;
+    }
+
+    public void setBirth(String birth) {
+        this.birth = birth;
+    }
+
+    public String getfName() {
+        return fName;
+    }
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    public String getlName() {
+        return lName;
+    }
+
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
+
+    public String getNationalID() {
+        return nationalID;
+    }
+
+    public void setNationalID(String nationalID) {
+        this.nationalID = nationalID;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getBloodGroup() {
+        return bloodGroup;
+    }
+
+    public void setBloodGroup(String bloodGroup) {
+        this.bloodGroup = bloodGroup;
+    }
+
+    public DonatorProfile(String birth, String fName, String lName, String nationalID, String email, String bloodGroup) {
+        this.birth = birth;
+        this.fName = fName;
+        this.lName = lName;
+        this.nationalID = nationalID;
+        this.email = email;
+        this.bloodGroup = bloodGroup;
+    }
+}
