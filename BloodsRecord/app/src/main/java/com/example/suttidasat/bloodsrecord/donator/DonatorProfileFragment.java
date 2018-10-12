93926585eead7f0c7e2362633bcd92a812862602
JugJig <59070027@it.kmitl.ac.th>
2018-10-13 01:08:45 +0700

    Update BloodsRecord/app/src/main/java/com/example/suttidasat/bloodsrecord/donator/DonatorProfileFragment.java

--- BloodsRecord/app/src/main/java/com/example/suttidasat/bloodsrecord/donator/DonatorProfileFragment.java
+++ BloodsRecord/app/src/main/java/com/example/suttidasat/bloodsrecord/donator/DonatorProfileFragment.java
@@ -9,6 +9,12 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.suttidasat.bloodsrecord.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class DonatorProfileFragment extends Fragment {
 
@@ -18,8 +24,32 @@ public class DonatorProfileFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_donator_profile,container,false);
     }
 
+    //Firebase
+    FirebaseFirestore firestore;
+    FirebaseAuth fbAuth;
+
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+
+        //Firebase
+        firestore = FirebaseFirestore.getInstance();
+        fbAuth = FirebaseAuth.getInstance();
+
+        //GET VALUDE FROM FIREBASE
+        String user = fbAuth.getCurrentUser().getUid();
+        firestore.collection("bloodsRecord")
+                .document(user).addSnapshotListener(getActivity(), new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(@javax.annotation.Nullable QuerySnapshot queryDocumentSnapshots, @javax.annotation.Nullable FirebaseFirestoreException e) {
+                for (DocumentSnapshot d: queryDocumentSnapshots.getDocuments()){
+
+                }
+            }
+        });
+
     }
+
+
+
 }
