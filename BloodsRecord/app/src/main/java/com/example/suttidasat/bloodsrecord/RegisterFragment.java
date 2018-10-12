ac7c12cf45c13d43ef0d9dc7719a0d596d13f922
JugJig <59070027@it.kmitl.ac.th>
2018-10-12 23:24:04 +0700

    Donator Data saved in firebase
    complete 100 %

--- BloodsRecord/app/src/main/java/com/example/suttidasat/bloodsrecord/RegisterFragment.java
+++ BloodsRecord/app/src/main/java/com/example/suttidasat/bloodsrecord/RegisterFragment.java
@@ -13,11 +13,18 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.example.suttidasat.bloodsrecord.donator.DonatorProfile;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreSettings;
 
@@ -30,7 +37,7 @@ public class RegisterFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_register, container,false);
     }
 
-
+    //Firebase
     private FirebaseAuth fbAuth;
     private FirebaseFirestore firestore;
 
@@ -38,15 +45,13 @@ public class RegisterFragment extends Fragment {
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
+        //Firebase
         fbAuth = FirebaseAuth.getInstance();
         firestore = FirebaseFirestore.getInstance();
-
-        FirebaseFirestore firestore = FirebaseFirestore.getInstance();
-        FirebaseFirestoreSettings settings = new FirebaseFirestoreSettings.Builder()
-                .setTimestampsInSnapshotsEnabled(true)
-                .build();
-        firestore.setFirestoreSettings(settings);
-
+//        FirebaseFirestoreSettings settings = new FirebaseFirestoreSettings.Builder()
+//                .setTimestampsInSnapshotsEnabled(true)
+//                .build();
+//        firestore.setFirestoreSettings(settings);
 
 
         registerBtn();
@@ -69,12 +74,12 @@ public class RegisterFragment extends Fragment {
                 EditText rePasswordEdt = getView().findViewById(R.id.registerRePassword);
 
                 //CONVERSE TO STRING
-                  final String firstnameStr = firstnameEdt.getText().toString();
-                  final String lastnameStr = lastnameEdt.getText().toString();
-                  final String birthStr = birthEdt.getText().toString();
-                  final String nationalIDStr = nationalIDEdt.getText().toString();
-                  final String bloodsStr = bloodsEdt.getText().toString();
-                  final String emailStr = emailEdt.getText().toString();
+                   final String firstnameStr = firstnameEdt.getText().toString();
+                   final String lastnameStr = lastnameEdt.getText().toString();
+                   final String birthStr = birthEdt.getText().toString();
+                   final String nationalIDStr = nationalIDEdt.getText().toString();
+                   final String bloodsStr = bloodsEdt.getText().toString();
+                   final String emailStr = emailEdt.getText().toString();
                 String passwordStr = passwordEdt.getText().toString();
                 String rePasswordStr = rePasswordEdt.getText().toString();
 
@@ -87,12 +92,11 @@ public class RegisterFragment extends Fragment {
                     Log.d("REGISTER", "PASSWORD IS NOT EQUALS REPASSWORD");
                     Toast.makeText(getActivity(),"รหัสผ่านไม่ถูกต้อง",Toast.LENGTH_SHORT).show();
                 }else{
-                    // check verify email
-                    fbAuth.createUserWithEmailAndPassword(emailStr,passwordStr).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                    fbAuth.createUserWithEmailAndPassword(emailStr,passwordStr).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                         @Override
-                        public void onSuccess(AuthResult authResult) {
-                            String uid = fbAuth.getCurrentUser().getUid();
+                        public void onComplete(@NonNull Task<AuthResult> task) {
 
+                            String uid = fbAuth.getCurrentUser().getUid();
 //                            String birth, String fName, String lName, String nationalID, String email, String bloodGroup
                             DonatorProfile dp = new DonatorProfile(birthStr,firstnameStr,lastnameStr,nationalIDStr,emailStr
                             ,bloodsStr);
@@ -100,12 +104,20 @@ public class RegisterFragment extends Fragment {
                             Log.d("REGISTER", "REGISTER SUCCESS");
                             firestore.collection("bloodsRecord")
                                     .document(uid)
-                                    .collection("nationalID")
-                                    .document(nationalIDStr)
                                     .set(dp).addOnSuccessListener(new OnSuccessListener<Void>() {
                                 @Override
                                 public void onSuccess(Void aVoid) {
                                     Log.d("REGISTER", "VALUE HAS BEEN SAVED IN FIREBASE");
+
+                                    FirebaseAuth.getInstance().signOut();
+
+                                    getActivity().getSupportFragmentManager()
+                                            .beginTransaction()
+                                            .replace(R.id.main_view, new LoginFragment())
+                                            .addToBackStack(null)
+                                            .commit();
+                                    Log.d("USER", "YOU HAS BEEN SIGN OUT");
+                                    Log.d("USER", "GOTO LOGIN");
                                 }
                             }).addOnFailureListener(new OnFailureListener() {
                                 @Override
@@ -114,80 +126,47 @@ public class RegisterFragment extends Fragment {
                                     Toast.makeText(getContext(),"ERROR = "+e.getMessage(),Toast.LENGTH_SHORT).show();
                                 }
                             });
-
-                            //save donator profile into firebase
-//                            firebaseDonatorProfile(birthStr,firstnameStr,lastnameStr,nationalIDStr
-//                                    ,emailStr,bloodsStr);
-                            //sentverifedemail
-                            sendVerifiedEmail(authResult.getUser());
-                        }
-                    }).addOnFailureListener(new OnFailureListener() {
-                        @Override
-                        public void onFailure(@NonNull Exception e) {
-                            Log.d("REGISTER", "ERROR =" + e);
-                            Toast.makeText(getActivity(),"Error = " + e, Toast.LENGTH_LONG).show();
                         }
                     });
+
+
                 }
             }
         });
     }
 
-//    private void firebaseDonatorProfile(String birth, String fName, String lName, String nationalID, String email, String bloodGroup) {
-//        String uid = fbAuth.getCurrentUser().getUid();
 //
-////                            String birth, String fName, String lName, String nationalID, String email, String bloodGroup
-//        DonatorProfile dp = new DonatorProfile(birth,fName,lName,nationalID
-//                ,email,bloodGroup);
-//
-//        firestore.collection("bloodsRecord")
-//                .document(uid)
-//                .set(dp).addOnSuccessListener(new OnSuccessListener<Void>() {
+//    void sendVerifiedEmail(FirebaseUser _user){
+//        _user.sendEmailVerification().addOnSuccessListener(new OnSuccessListener<Void>() {
 //            @Override
 //            public void onSuccess(Void aVoid) {
-//                Log.d("REGISTER", "VALUE HAS BEEN SAVED IN FIREBASE");
+//                Toast.makeText(getActivity(),"YOU HAS BEEN REGISTER PLEASE CONFIRM YOUR E-MAIL"
+//                        ,Toast.LENGTH_SHORT)
+//                        .show();
+//
+//                getActivity().getSupportFragmentManager()
+//                        .beginTransaction()
+//                        .replace(R.id.main_view,new LoginFragment())
+//                        .commit();
+//
+////                 user sign out
+//                FirebaseAuth.getInstance().signOut();
+//                getActivity().getSupportFragmentManager()
+//                        .beginTransaction()
+//                        .replace(R.id.main_view, new LoginFragment())
+//                        .addToBackStack(null)
+//                        .commit();
+//                Log.d("USER", "YOU HAS BEEN SIGN OUT");
+//                Log.d("USER", "GOTO LOGIN");
 //            }
 //        }).addOnFailureListener(new OnFailureListener() {
 //            @Override
 //            public void onFailure(@NonNull Exception e) {
 //                Log.d("REGISTER", "ERRROR =" + e.getMessage());
-//                Toast.makeText(getContext(),"ERROR = "+e.getMessage(),Toast.LENGTH_SHORT).show();
+//                Toast.makeText(getActivity(),"ERROR = " + e.getMessage()
+//                        ,Toast.LENGTH_SHORT)
+//                        .show();
 //            }
 //        });
 //    }
-
-
-    void sendVerifiedEmail(FirebaseUser _user){
-        _user.sendEmailVerification().addOnSuccessListener(new OnSuccessListener<Void>() {
-            @Override
-            public void onSuccess(Void aVoid) {
-                Toast.makeText(getActivity(),"YOU HAS BEEN REGISTER PLEASE CONFIRM YOUR E-MAIL"
-                        ,Toast.LENGTH_SHORT)
-                        .show();
-
-                getActivity().getSupportFragmentManager()
-                        .beginTransaction()
-                        .replace(R.id.main_view,new LoginFragment())
-                        .commit();
-
-//                 user sign out
-                FirebaseAuth.getInstance().signOut();
-                getActivity().getSupportFragmentManager()
-                        .beginTransaction()
-                        .replace(R.id.main_view, new LoginFragment())
-                        .addToBackStack(null)
-                        .commit();
-                Log.d("USER", "YOU HAS BEEN SIGN OUT");
-                Log.d("USER", "GOTO LOGIN");
-            }
-        }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                Log.d("REGISTER", "ERRROR =" + e.getMessage());
-                Toast.makeText(getActivity(),"ERROR = " + e.getMessage()
-                        ,Toast.LENGTH_SHORT)
-                        .show();
-            }
-        });
-    }
 }
