2c8abbbd0bf56ec83196ce3aaeff571b45ad91ca
Suttida Sat <59070178@kmitl.ac.th>
2018-10-13 21:19:03 +0700

    เปลี่ยนหน้า

--- BloodsRecord/app/src/main/java/com/example/suttidasat/bloodsrecord/InsertHistory.java
+++ BloodsRecord/app/src/main/java/com/example/suttidasat/bloodsrecord/InsertHistory.java
@@ -19,7 +19,7 @@ public class InsertHistory extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_weight_form, container,false);
+        return inflater.inflate(R.layout.insert_history, container,false);
     }
 
     @Override
