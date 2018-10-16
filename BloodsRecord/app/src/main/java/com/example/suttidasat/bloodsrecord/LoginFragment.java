9d9a8f5c7969ff4b1992697d345b0b1781f34974
mnchmrw <59070045@it.kmitl.ac.th>
2018-10-16 13:05:03 +0700

    edit test

--- BloodsRecord/app/src/main/java/com/example/suttidasat/bloodsrecord/LoginFragment.java
+++ BloodsRecord/app/src/main/java/com/example/suttidasat/bloodsrecord/LoginFragment.java
@@ -28,7 +28,7 @@ public class LoginFragment extends Fragment {
              @Nullable Bundle savedInstanceState) {
 
         //
-        return inflater.inflate(R.layout.fragment_login, container, false);
+        return inflater.inflate(R.layout.fragment_login,container,false);
     }
 
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
