fa0cf3be89f132598186c3902a3c95501df8516f
Suttida Sat <59070178@kmitl.ac.th>
2018-10-20 12:41:49 +0700

    แก้ DB Hostory

--- BloodsRecord/app/src/main/java/com/example/suttidasat/bloodsrecord/model/DonatorHistory.java
+++ BloodsRecord/app/src/main/java/com/example/suttidasat/bloodsrecord/model/DonatorHistory.java
@@ -1,31 +1,31 @@
 package com.example.suttidasat.bloodsrecord.model;
 
 public class DonatorHistory  {
-    //    private String date;
-    private int num;
+        private String date;
+//    private int num;
 
     public DonatorHistory() {
     }
 
-    public  DonatorHistory(int num){
+    public  DonatorHistory(String date){
 
-//        this.date = date;
-        this.num = num;
+        this.date = date;
+//        this.num = num;
     }
 
-//    public String getDate() {
-//        return date;
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+//    public int getNum() {
+//        return num;
 //    }
 //
-//    public void setDate(String date) {
-//        this.date = date;
+//    public void setNum(int num) {
+//        this.num = num;
 //    }
-
-    public int getNum() {
-        return num;
-    }
-
-    public void setNum(int num) {
-        this.num = num;
-    }
 }
