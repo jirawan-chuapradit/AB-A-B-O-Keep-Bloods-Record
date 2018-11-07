cb6f69766104bc75166afc5860f4f9460c6f68ed
Suttida Sat <59070178@kmitl.ac.th>
2018-11-08 00:24:06 +0700

    ดูข่าว จนท.

--- BloodsRecord/app/src/main/java/com/example/suttidasat/bloodsrecord/Interface/ViewNews.java
+++ BloodsRecord/app/src/main/java/com/example/suttidasat/bloodsrecord/Interface/ViewNews.java
@@ -7,13 +7,39 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import com.example.suttidasat.bloodsrecord.R;
+import com.example.suttidasat.bloodsrecord.adapter.NewsAdapter;
+import com.example.suttidasat.bloodsrecord.model.ConnectDB;
+import com.example.suttidasat.bloodsrecord.model.News;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
 
 public class ViewNews extends Fragment {
+
+    FirebaseFirestore firestore = ConnectDB.getConnect();
+    ArrayList<News> news = new ArrayList<>();
+    ListView newList;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_view_news,container,false);
+    }
+
+
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        newList = getView().findViewById(R.id.news_list);
+
+        final NewsAdapter _weightAdapter = new NewsAdapter(
+                getActivity(),
+                R.layout.manage_news_item,
+                news
+        );
     }
 }
---
+++ BloodsRecord/app/src/main/java/com/example/suttidasat/bloodsrecord/model/News.java
@@ -0,0 +1,43 @@
+package com.example.suttidasat.bloodsrecord.model;
+
+public class News {
+
+    private String title;
+    private String detail;
+    private String date;
+    private String link;
+
+    public void News(){}
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDetail() {
+        return detail;
+    }
+
+    public void setDetail(String detail) {
+        this.detail = detail;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+}
