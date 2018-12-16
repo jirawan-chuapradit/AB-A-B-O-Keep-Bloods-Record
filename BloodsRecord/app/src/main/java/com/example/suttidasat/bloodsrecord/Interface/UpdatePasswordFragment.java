2bcdc20ad7e89c14dc3135295589e59b78822fe5
JugJig <59070027@it.kmitl.ac.th>
2018-12-16 13:14:12 +0700

    Revert "Update UpdatePasswordFragment.java"
    This reverts commit b68e61dc79def2cb62c9fa08ac398046cd0f43d1.

--- BloodsRecord/app/src/main/java/com/example/suttidasat/bloodsrecord/Interface/UpdatePasswordFragment.java
+++ BloodsRecord/app/src/main/java/com/example/suttidasat/bloodsrecord/Interface/UpdatePasswordFragment.java
@@ -7,6 +7,8 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -25,6 +27,8 @@ import android.widget.Toast;
 
 import com.example.suttidasat.bloodsrecord.MainActivity;
 import com.example.suttidasat.bloodsrecord.R;
+import com.example.suttidasat.bloodsrecord.init.BloodsRecordFirebase;
+import com.example.suttidasat.bloodsrecord.model.DonatorProfile;
 import com.example.suttidasat.bloodsrecord.model.MyService;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
@@ -34,7 +38,9 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
 
 /*************************************************
  *intent: Update password                        *
